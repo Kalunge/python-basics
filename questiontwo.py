671e7a95a686302e66268cc2aaf14f6a91169dde
kalunge <muthomititus+github@gmail.com>
2021-05-24 15:43:14 +0300

    question two

---
+++ questiontwo.py
@@ -0,0 +1,9 @@
+sum = 0
+count = int(input("Enter count "))
+for num in range(count):
+    sum += num
+
+average = sum / count
+
+print(f"The sum of the number is {sum}" )
+print(f"The average of the number is {average}" )
