155e25bb02818facffa15bed15f2b2fc69780282
kalunge <muthomititus+github@gmail.com>
2021-05-24 15:22:41 +0300

    initial commit

---
+++ even.py
@@ -0,0 +1,12 @@
+sum = 0
+for num in range(1000, 2000):
+    if num % 2 == 0:
+        sum += num
+
+print(sum)
+
+# declare a variable called sum or total SUM = 0
+# we generate numbers using the range function RANGE(MIN_LIMIT, MAXIMUM_LIMIT)
+# we check for even numbers NUMBER % 2 == 0
+# we add each even number to the sum/total SUM = SUM + NUMBER
+# print the sum/total PRINT(SUM)
